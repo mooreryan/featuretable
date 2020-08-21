9d04103dfc40321416bfa296ba033728a9a9947e
Ryan Moore <moorer@udel.edu>
2020-08-21 01:23:28 -0400

    Refactor data handling functions

--- R/feature_table.R
+++ R/feature_table.R
@@ -51,124 +51,131 @@ FeatureTable <- R6::R6Class(
       self$dim <- dim(self$data)
 
       #### Set the feature data ####
-
       if (!is.null(feature_data)) {
-        if (length(dim(feature_data)) > 2) {
-          rlang::abort("Dim of feature_data was > 2.", class = Error$ArgumentError)
-        }
-
-        # Check if data is some sort of 1d thing.
-        if (is.null(dim(feature_data))) {
-          # Yeah, it's 1d.
-
-          # If there are no names, then we abort.
-          if (is.null(names(feature_data))) {
-            rlang::abort("1d feature_data had no names attribute",
-                         class = Error$ArgumentError)
-          }
-
-          # Make sure that there are at least some names in common.
-          if (!any(self$feature_names %in% names(feature_data))) {
-            rlang::abort("None of the feature names in feature_table were found in feature_data. Check your input!", class = Error$ArgumentError)
-          }
-
-          # Create a data frame from the 1d data.
-          self$feature_data <- data.frame(X = feature_data[self$feature_names],
-                                          row.names = seq_len(self$num_features))
-        }
-        # At least we have a 3d structure....
-        else {
-          # Check if any dimension is zero.  Lots of checks for better error messages.
-          if (nrow(feature_data) == 0 | ncol(feature_data) == 0) {
-            rlang::abort("feature_data had zero rows and columns!", class = Error$ArgumentError)
-          }
-
-          if (nrow(feature_data) == 0) {
-            rlang::abort("feature_data had zero rows!", class = Error$ArgumentError)
-          }
-
-          if (ncol(feature_data) == 0) {
-            rlang::abort("feature_data had zero columns!", class = Error$ArgumentError)
-          }
-
-          if (!any(self$feature_names %in% rownames(feature_data))) {
-            rlang::abort("None of the feature names in the feature_table were found in the feature_data. Check your input!",
-                         class = Error$ArgumentError)
-          }
-
-          # Even if it is a data frame, it still may have 1 covariate, which needs special treatment.
-          if (ncol(feature_data) == 1) {
-            self$feature_data <- data.frame(X = feature_data[self$feature_names, ])
-            colnames(self$feature_data) <- colnames(feature_data)[[1]]
-          } else if (ncol(feature_data) > 1) {
-            self$feature_data <- feature_data[self$feature_names, ]
-          } else {
-            rlang::abort("it should be impossible to get here...",
-                         class = Error$ImpossibleConditionError)
-          }
-        }
-
-        # Ensure correct rownames are there in case feature_data row names didn't match up.
-        rownames(self$feature_data) <- colnames(self$data)
+        private$handle_feature_data(feature_data)
       }
 
       #### Set the sample data ####
-
       if (!is.null(sample_data)) {
-        if (length(dim(sample_data)) > 2) {
-          rlang::abort("Dim of sample_data was > 2.", class = Error$ArgumentError)
-        }
-
-        # Data is some sort of vector.
-        if (is.null(dim(sample_data))) {
-          if (is.null(names(sample_data))) {
-            rlang::abort("1d sample_data had no names attribute", class = Error$ArgumentError)
-          }
-
-          if (!any(self$sample_names %in% names(sample_data))) {
-            rlang::abort("None of the sample names in feature_table were found in sample_data.  Check your input!",
-                         class = Error$ArgumentError)
-          }
-
-          self$sample_data <- data.frame(X = sample_data[self$sample_names],
-                                         # Need to set this manually.  If more samples in feature_table
-                                         # than in sample_data, you will get some missing row names here.
-                                         row.names = 1:self$num_samples)
-        } else {
-          # At least we have a 2d structure.
-
-          # Check if any dimension is zero.  Lots of checks for better error messages.
-          if (nrow(sample_data) == 0 | ncol(sample_data) == 0) {
-            rlang::abort("sample_data had zero rows and columns!", class = Error$ArgumentError)
-          }
-
-          if (nrow(sample_data) == 0) {
-            rlang::abort("sample_data had zero rows!", class = Error$ArgumentError)
-          }
-
-          if (ncol(sample_data) == 0) {
-            rlang::abort("sample_data had zero columns!", class = Error$ArgumentError)
-          }
-
-          if (!any(self$sample_names %in% rownames(sample_data))) {
-            rlang::abort("None of the sample names in feature_table were found in sample_data.  Check your input!",
-                         class = Error$ArgumentError)
-          }
-
-          if (ncol(sample_data) == 1) {
-            self$sample_data <- data.frame(X = sample_data[self$sample_names, ])
-            colnames(self$sample_data) <- colnames(sample_data)[[1]]
-          } else if (ncol(sample_data) > 1) {
-            self$sample_data <- sample_data[self$sample_names, ]
-          } else {
-            rlang::abort("it should be impossible to get here...",
-                         class = Error$ImpossibleConditionError)
-          }
-        }
-
-        # Ensure correct rownames are there in case there are more samples in feature_table than sample_data.
-        rownames(self$sample_data) <- rownames(self$data)
+        private$handle_sample_data(sample_data)
       }
+    }
+  ),
+  private = list(
+    handle_feature_data = function(feature_data) {
+      if (length(dim(feature_data)) > 2) {
+        rlang::abort("Dim of feature_data was > 2.", class = Error$ArgumentError)
+      }
+
+      # Check if data is some sort of 1d thing.
+      if (is.null(dim(feature_data))) {
+        # Yeah, it's 1d.
+
+        # If there are no names, then we abort.
+        if (is.null(names(feature_data))) {
+          rlang::abort("1d feature_data had no names attribute",
+                       class = Error$ArgumentError)
+        }
+
+        # Make sure that there are at least some names in common.
+        if (!any(self$feature_names %in% names(feature_data))) {
+          rlang::abort("None of the feature names in feature_table were found in feature_data. Check your input!", class = Error$ArgumentError)
+        }
+
+        # Create a data frame from the 1d data.
+        self$feature_data <- data.frame(X = feature_data[self$feature_names],
+                                        row.names = seq_len(self$num_features))
+      }
+      # At least we have a 3d structure....
+      else {
+        # Check if any dimension is zero.  Lots of checks for better error messages.
+        if (nrow(feature_data) == 0 | ncol(feature_data) == 0) {
+          rlang::abort("feature_data had zero rows and columns!", class = Error$ArgumentError)
+        }
+
+        if (nrow(feature_data) == 0) {
+          rlang::abort("feature_data had zero rows!", class = Error$ArgumentError)
+        }
+
+        if (ncol(feature_data) == 0) {
+          rlang::abort("feature_data had zero columns!", class = Error$ArgumentError)
+        }
+
+        if (!any(self$feature_names %in% rownames(feature_data))) {
+          rlang::abort("None of the feature names in the feature_table were found in the feature_data. Check your input!",
+                       class = Error$ArgumentError)
+        }
+
+        # Even if it is a data frame, it still may have 1 covariate, which needs special treatment.
+        if (ncol(feature_data) == 1) {
+          self$feature_data <- data.frame(X = feature_data[self$feature_names, ])
+          colnames(self$feature_data) <- colnames(feature_data)[[1]]
+        } else if (ncol(feature_data) > 1) {
+          self$feature_data <- feature_data[self$feature_names, ]
+        } else {
+          rlang::abort("it should be impossible to get here...",
+                       class = Error$ImpossibleConditionError)
+        }
+      }
+
+      # Ensure correct rownames are there in case feature_data row names didn't match up.
+      rownames(self$feature_data) <- colnames(self$data)
+    },
+
+    handle_sample_data = function(sample_data) {
+      if (length(dim(sample_data)) > 2) {
+        rlang::abort("Dim of sample_data was > 2.", class = Error$ArgumentError)
+      }
+
+      # Data is some sort of vector.
+      if (is.null(dim(sample_data))) {
+        if (is.null(names(sample_data))) {
+          rlang::abort("1d sample_data had no names attribute", class = Error$ArgumentError)
+        }
+
+        if (!any(self$sample_names %in% names(sample_data))) {
+          rlang::abort("None of the sample names in feature_table were found in sample_data.  Check your input!",
+                       class = Error$ArgumentError)
+        }
+
+        self$sample_data <- data.frame(X = sample_data[self$sample_names],
+                                       # Need to set this manually.  If more samples in feature_table
+                                       # than in sample_data, you will get some missing row names here.
+                                       row.names = 1:self$num_samples)
+      } else {
+        # At least we have a 2d structure.
+
+        # Check if any dimension is zero.  Lots of checks for better error messages.
+        if (nrow(sample_data) == 0 | ncol(sample_data) == 0) {
+          rlang::abort("sample_data had zero rows and columns!", class = Error$ArgumentError)
+        }
+
+        if (nrow(sample_data) == 0) {
+          rlang::abort("sample_data had zero rows!", class = Error$ArgumentError)
+        }
+
+        if (ncol(sample_data) == 0) {
+          rlang::abort("sample_data had zero columns!", class = Error$ArgumentError)
+        }
+
+        if (!any(self$sample_names %in% rownames(sample_data))) {
+          rlang::abort("None of the sample names in feature_table were found in sample_data.  Check your input!",
+                       class = Error$ArgumentError)
+        }
+
+        if (ncol(sample_data) == 1) {
+          self$sample_data <- data.frame(X = sample_data[self$sample_names, ])
+          colnames(self$sample_data) <- colnames(sample_data)[[1]]
+        } else if (ncol(sample_data) > 1) {
+          self$sample_data <- sample_data[self$sample_names, ]
+        } else {
+          rlang::abort("it should be impossible to get here...",
+                       class = Error$ImpossibleConditionError)
+        }
+      }
+
+      # Ensure correct rownames are there in case there are more samples in feature_table than sample_data.
+      rownames(self$sample_data) <- rownames(self$data)
     }
   )
 )
