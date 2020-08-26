a5ea46e8f53566b542cf57979bff86a89bf2300a
Ryan Moore <moorer@udel.edu>
2020-08-25 20:57:51 -0400

    Add map s3 functions and tests

--- R/feature_table_s3.R
+++ R/feature_table_s3.R
@@ -37,3 +37,83 @@ reduce_all.FeatureTable <- function(ft, fn, ...) {
   ft$reduce_all(fn, ...)
 }
 
+################################################################################
+#### map #######################################################################
+################################################################################
+
+map <- function(x, ...) {
+  UseMethod("map")
+}
+
+map.FeatureTable <- function(ft, margin, fn, ...) {
+  ft$map(margin, fn, ...)
+}
+
+map_with_index <- function(x, ...) {
+  UseMethod("map_with_index")
+}
+
+map_with_index.FeatureTable <- function(ft, margin, fn, ...) {
+  ft$map_with_index(margin, fn, ...)
+}
+
+map_with_name <- function(x, ...) {
+  UseMethod("map_with_name")
+}
+
+map_with_name.FeatureTable <- function(ft, margin, fn, ...) {
+  ft$map_with_name(margin, fn, ...)
+}
+
+
+#### map_features ####
+
+map_features <- function(x, ...) {
+  UseMethod("map_features")
+}
+
+map_features.FeatureTable <- function(ft, fn, ...) {
+  ft$map_features(fn, ...)
+}
+
+map_features_with_index <- function(x, ...) {
+  UseMethod("map_features_with_index")
+}
+
+map_features_with_index.FeatureTable <- function(ft, fn, ...) {
+  ft$map_features_with_index(fn, ...)
+}
+
+map_features_with_name <- function(x, ...) {
+  UseMethod("map_features_with_name")
+}
+
+map_features_with_name.FeatureTable <- function(ft, fn, ...) {
+  ft$map_features_with_name(fn, ...)
+}
+
+#### map_features ####
+
+map_samples <- function(x, ...) {
+  UseMethod("map_samples")
+}
+
+map_samples.FeatureTable <- function(ft, fn, ...) {
+  ft$map_samples(fn, ...)
+}
+
+map_samples_with_index <- function(x, ...) {
+  UseMethod("map_samples_with_index")
+}
+
+map_samples_with_index.FeatureTable <- function(ft, fn, ...) {
+  ft$map_samples_with_index(fn, ...)
+}
+
+map_samples_with_name <- function(x, ...) {
+  UseMethod("map_samples_with_name")
+}
+
+map_samples_with_name.FeatureTable <- function(ft, fn, ...) {
+  ft$map_samples_with_name(fn, ...)
+}
