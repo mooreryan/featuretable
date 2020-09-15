11eeccaf644c711f6d708eb7e6bcabca72058b2e
Ryan Moore <moorer@udel.edu>
2020-09-15 01:48:40 -0400

    Refactor plotting functions and add some docs

--- R/feature_table_s3.R
+++ R/feature_table_s3.R
@@ -341,18 +341,6 @@ clr.FeatureTable <- function(ft, base = 2) {
 }
 
 ################################################################################
-#### ordination ################################################################
-################################################################################
-
-pca_biplot <- function(ft, use_biplotr = FALSE, include_sample_data = FALSE, ...) {
-  UseMethod("pca_biplot")
-}
-
-pca_biplot.FeatureTable <- function(ft, use_biplotr = FALSE, include_sample_data = FALSE, ...) {
-  ft$pca_biplot(use_biplotr = use_biplotr, include_sample_data = include_sample_data, ...)
-}
-
-################################################################################
 #### importing data ############################################################
 ################################################################################
 
@@ -536,3 +524,47 @@ shared_feature_names <- function(ft, other) {
 shared_feature_names.FeatureTable <- function(ft, other) {
   ft$shared_feature_names(other)
 }
+
+################################################################################
+#### plotting ##################################################################
+################################################################################
+
+#' FeatureTable summary plots
+#'
+#' @param ft A FeatureTable
+#' @param num_features How many features to show on the plot? The rest of the
+#'   features get put into an "Other" category. "Other" will always be gray if
+#'   you use one of the built in palettes.
+#' @param fill If FALSE, don't show the features, if anything else, show the
+#'   features.
+#' @param palette Which palette to use? ("kelly", "muted", "bright", "vibrant",
+#'    "high contrast").  If there are more features than colors in the palette,
+#'     the colors will be recycled.
+#' @param show_legend Show the legend on the right side?
+#' @param legend_title Custom legend title
+#' @param plot_title Custom plot title
+#' @param xlab Custom xlab
+#' @param ylab Custom ylab
+#' @param axis.text.x Custom \code{axis.text.x}.  Should be an
+#'    \code{element_text}.
+#' @param ... Extra args
+#'
+#' @return A `ggplot2` object.
+#'
+#' @export
+plot.FeatureTable <- function(ft, ...) {
+  ft$plot(...)
+}
+
+################################################################################
+#### ordination ################################################################
+################################################################################
+
+pca_biplot <- function(ft, use_biplotr = FALSE, include_sample_data = FALSE, ...) {
+  UseMethod("pca_biplot")
+}
+
+pca_biplot.FeatureTable <- function(ft, use_biplotr = FALSE, include_sample_data = FALSE, ...) {
+  ft$pca_biplot(use_biplotr = use_biplotr, include_sample_data = include_sample_data, ...)
+}
+
